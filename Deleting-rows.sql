0edf1694c4e23382c6446724dc9794cddf0845fb
Karla Dinorah Martinez Trasviña <karlamartinez82140@gmail.com>
2026-05-11 22:32:51 -0700

    Delete movies based on release year and director
    Remove movies released before 2005 and directed by Andrew Stanton.

---
+++ Deleting-rows.sql
@@ -0,0 +1,9 @@
+--Deleting rows--
+
+/*This database is getting too big, lets remove all movies that were released before 2005.*/
+
+delete from Movies where year < 2005
+
+/*Andrew Stanton has also left the studio, so please remove all movies directed by him.*/
+
+delete from movies where director = 'Andrew Stanton';
