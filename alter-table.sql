efdbdc0f3f82ab0e8e409e40dc318f031ce34d16
Karla Dinorah Martinez Trasviña <karlamartinez82140@gmail.com>
2026-05-13 19:45:11 -0700

    alter tables

---
+++ alter-table.sql
@@ -0,0 +1,11 @@
+--Altering tables--
+
+/*Add a column named Aspect_ratio with a FLOAT data type to store 
+the aspect-ratio each movie was released in.*/
+
+alter table Movies add Aspect_ratio FLOAT;
+
+/* Add another column named Language with a TEXT data type to store the language 
+that the movie was released in. Ensure that the default for this language is English. */
+
+alter table Movies add Language TEXT default 'English';
