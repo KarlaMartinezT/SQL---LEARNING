c24fa03cc6d1a49ffa5ec33dd95bf9814fcfdcc1
Karla Dinorah Martinez Trasviña <karlamartinez82140@gmail.com>
2026-05-13 19:57:37 -0700

    Drop Movies and BoxOffice tables
    Remove Movies and BoxOffice tables as part of cleanup.

---
+++ drop-tables.sql
@@ -0,0 +1,9 @@
+-- Dropping tables --
+
+/*We've sadly reached the end of our lessons, lets clean up by removing the Movies table*/
+
+drop table if exists Movies;
+
+/* And drop the BoxOffice table as well */
+
+drop table if exists BoxOffice;
